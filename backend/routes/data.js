3c87bfca7b16f3833ece62e7a4fc958e609dbb10
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-08 16:05:45 +0545

    fixed routing issue at backend

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getAllData, getAllDataTesting } = require("../controllers/data");
 
-router.route("/array").get(getAllData);
+router.route("/").get(getAllData);
 router.route("/testing").get(getAllDataTesting);
 
 module.exports = router;

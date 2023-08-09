3b4e6a2d8b7d76ca823089af0ae974fd3e711df8
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-09 16:39:09 +0545

    made the pagination on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = 8080;
+const usersData = require("./data"); // Import user data from data.js
+
 const app = express();
+const PORT = 8080;
 
 app.use(cors());
 
-const data_routes = require("./routes/data");
+// Define an endpoint to handle paginated user data
+app.get("/api/data/users", (req, res) => {
+  const { page = 1, perPage = 5 } = req.query;
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  const users = usersData.slice(startIndex, endIndex);
 
-app.get("/array", (req, res) => {
-  res.send("noice");
+  // Calculate the total count of users
+  const totalCount = usersData.length;
+
+  res.setHeader("X-Total-Count", totalCount);
+
+  // Equivalent of _.range(1, totalPage + 1) using native JavaScript
+  const totalPage = Math.ceil(totalCount / perPage);
+  const paginationRange = Array.from(
+    { length: totalPage },
+    (_, index) => index + 1
+  );
+
+  res.json({
+    users,
+    paginationRange,
+  });
 });
 
-//middleware or to set router
-app.use("/api/data", data_routes);
-
 app.listen(PORT, () => {
-  console.log(`listning to a port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -3,12 +3,27 @@ import React, { useState, useEffect } from "react";
 
 const Table = ({ users }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
-      .get("http://localhost:8080/api/data")
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
+      .get("http://localhost:8080/api/data/users")
+      .then((res) => {
+        setData(res.data.users);
+        setLoading(false); // Set loading to false after successful fetch
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false); // Set loading to false in case of an error
+      });
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!Array.isArray(data)) {
+    return <div>Data is not in the expected format.</div>;
+  }
   return (
     <div>
       <table className="table text-center">
@@ -22,9 +37,9 @@ const Table = ({ users }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((user, index) => {
+          {users.map((user) => {
             return (
-              <tr key={index}>
+              <tr key={user.id}>
                 <td>{user.id}</td>
                 <td>{user.fullName}</td>
                 <td>{user.fullMobileNumber}</td>
